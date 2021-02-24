ebe4cdd123735a1a930946ab09ffaf9ab65561b2
Santiago Buitrago <brayan.buitrago@mail.escuelaing.edu.co>
2021-02-24 10:38:11 -0500

    Parte 1 Arreglada

--- covidAnalyzerApi/src/main/java/eci/arsw/covidanalyzer/CovidAggregateController.java
+++ covidAnalyzerApi/src/main/java/eci/arsw/covidanalyzer/CovidAggregateController.java
@@ -8,36 +8,115 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import static eci.arsw.covidanalyzer.model.ResultType.*;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import static org.springframework.web.bind.annotation.RequestMethod.PUT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
 @RestController
 public class CovidAggregateController {
     ICovidAggregateService covidAggregateService;
 
-    //TODO: Implemente todos los metodos POST que hacen falta.
+    @Autowired
+    @Qualifier("ICovidAggregateService")
+    ICovidAggregateService iCovidAggregateService;
 
+    //TODO: Implemente todos los metodos POST que hacen falta.
+    /**
+     * POST
+     * */
     @RequestMapping(value = "/covid/result/true-positive", method = RequestMethod.POST)
     public ResponseEntity addTruePositiveResult(Result result) {
-        //TODO
-        covidAggregateService.aggregateResult(result, ResultType.TRUE_POSITIVE);
-        return null;
+        try {
+            iCovidAggregateService.aggregateResult(result, TRUE_POSITIVE);
+        }catch (Exception e) {
+            return new ResponseEntity<>("ERROR 500", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @RequestMapping(value = "/covid/result/true-negative", method = RequestMethod.POST)
+    public ResponseEntity addTruePositiveResult(Result result) {
+        try {
+            iCovidAggregateService.aggregateResult(result, TRUE_NEGATIVE);
+        }catch (Exception e) {
+            return new ResponseEntity<>("ERROR 500", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @RequestMapping(value = "/covid/result/false-positive", method = RequestMethod.POST)
+    public ResponseEntity addTruePositiveResult(Result result) {
+        try {
+            iCovidAggregateService.aggregateResult(result, FALSE_POSITIVE);
+        }catch (Exception e) {
+            return new ResponseEntity<>("ERROR 500", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @RequestMapping(value = "/covid/result/false-negative", method = RequestMethod.POST)
+    public ResponseEntity addTruePositiveResult(Result result) {
+        try {
+            iCovidAggregateService.aggregateResult(result, ResultType.FALSE_NEGATIVE);
+        }catch (Exception e) {
+            return new ResponseEntity<>("ERROR 500", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
+    /**
+     * GET
+     * */
     //TODO: Implemente todos los metodos GET que hacen falta.
 
     @RequestMapping(value = "/covid/result/true-positive", method = RequestMethod.GET)
     public ResponseEntity getTruePositiveResult() {
-        //TODO
-        //covidAggregateService.getResult(ResultType.TRUE_POSITIVE);
-        return ResponseEntity.ok("Hello World");
+        try {
+            return new ResponseEntity<>(iCovidAggregateService.getResult(TRUE_POSITIVE), HttpStatus.ACCEPTED);
+        }catch (Exception e) {
+            return new ResponseEntity<>("ERROR 500", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @RequestMapping(value = "/covid/result/true-negative", method = RequestMethod.GET)
+    public ResponseEntity getTruePositiveResult() {
+        try {
+            return new ResponseEntity<>(iCovidAggregateService.getResult(TRUE_NEGATIVE), HttpStatus.ACCEPTED);
+        }catch (Exception e) {
+            return new ResponseEntity<>("ERROR 500", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @RequestMapping(value = "/covid/result/false-positive", method = RequestMethod.GET)
+    public ResponseEntity getTruePositiveResult() {
+        try {
+            return new ResponseEntity<>(iCovidAggregateService.getResult(FALSE_POSITIVE), HttpStatus.ACCEPTED);
+        }catch (Exception e) {
+            return new ResponseEntity<>("ERROR 500", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @RequestMapping(value = "/covid/result/false-negative", method = RequestMethod.GET)
+    public ResponseEntity getTruePositiveResult() {
+        try {
+            return new ResponseEntity<>(iCovidAggregateService.getResult(FALSE_NEGATIVE), HttpStatus.ACCEPTED);
+        }catch (Exception e) {
+            return new ResponseEntity<>("ERROR 500", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-
-    //TODO: Implemente el método.
+    /**
+     * PUT
+     * */
 
     @RequestMapping(value = "/covid/result/persona/{id}", method = RequestMethod.PUT)
     public ResponseEntity savePersonaWithMultipleTests() {
-        //TODO
-        covidAggregateService.getResult(ResultType.TRUE_POSITIVE);
+        try{
+            iCovidAggregateService.upsertPersonWithMultipleTests(result);
+            return new ResponseEntity<>(HttpStatus.ACCEPTED);
+        }catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
+        }
         return null;
     }
+
+
     
 }
